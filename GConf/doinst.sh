9548672ecb251b37b71f06f2e3638c3584fd62ec
Widya Walesa <walecha99@gmail.com>
2011-03-28 12:09:14 +0700

    GConf/doinst.sh     Removing unneeded command on doinst.sh script.

--- GConf/doinst.sh
+++ GConf/doinst.sh
@@ -15,20 +15,6 @@ config etc/gconf/2/path.new
 config etc/gconf/2/evoldap.conf.new
 config etc/dbus-1/system.d/org.gnome.GConf.Defaults.conf.new
 
-if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
-fi
-
-if [ -x /usr/bin/update-mime-database ]; then
-  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
-fi
-
-if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
-  if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
-  fi
-fi
-
 if [ -x usr/bin/glib-compile-schemas -a -d usr/share/glib-2.0/schemas ]; then
     usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas
 fi

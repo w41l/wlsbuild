6a29e406368baa3fcb5a28fcaea4caaa9519858e
w41l <walecha99@gmail.com>
2012-07-05 19:22:47 +0700

    * ffmpeg: Upgraded to 7.13. * grub: Upgraded to 2.00. * gsmartcontrol: Added version 0.8.6. Require hwinfo. * hwinfo: Added version 19.1. Require libx86emu. * kcm-grub2: Added version 0.5.8. Testing usage in Slackware. * libass: Upgraded to 0.10.0. * libbluray: Upgraded to 0.2.2. * libdvdcss: Upgraded to 1.2.12. * libkate: Upgraded to 0.4.1. * libx86emu: Added version 1.3. * mkvtoolnix: Upgraded to 5.6.0. Require libebml and libmatroska. * mozilla-aurora: Added version 15.0a2. Testing usage in Slackware. * opera: Upgraded to 12.00.1467. * vlc: Upgraded to 2.0.2. * x264: Upgraded to 35cf912. Added YUV4MPEG filter data. * xvidcore: Upgraded to 1.3.2.

--- avahi/doinst.sh
+++ avahi/doinst.sh
@@ -1,3 +1,4 @@
+
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
@@ -33,3 +34,4 @@ if [ -e /usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache -f -t /usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
+

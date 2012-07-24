651e6fadc15b7a2f06485c6dc97d4c6467ea422b
w41l <walecha99@gmail.com>
2012-07-24 21:43:02 +0700

    * aria2: Added version 1.15.1. Use by uget. * brasero: Upgraded to 3.4.1. * comix: Added version 4.0.4. * evince: Upgraded to 3.4.0. * gc: Upgraded to 7.2. * granite: Added but not failed to build. * hotot: Added version git c44d741. Need pygobject3. * icu4c: Upgraded to 4.8.1.1. Needed by webkitgtk. * libevent: Upgraded to 2.0.19. * libgee: Upgraded to 0.7.2. * midori: Upgraded to git f82a06a. Can not use Mozilla flashplayer plugin. * pygobject3: Added version 3.0.4. Needed by hotot. * python-xlib: Rebuilt. Use by hotot. * pywebkitgtk: Add info for failed to built. * transmission: Upgraded to 2.60. Needed libevent. * uget: Upgraded to 1.8.2. * vala: Upgraded to 0.16.1. * webkitgtk: Upgraded to 1.8.1. Enable webkit2 and gtk+3. * wine: Upgraded to 1.5.9.

--- evince/doinst.sh
+++ evince/doinst.sh
@@ -1,16 +1,3 @@
-schema_install() {
-  SCHEMA="$1"
-  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
-  chroot . gconftool-2 --makefile-install-rule \
-    /etc/gconf/schemas/$SCHEMA \
-    1>/dev/null
-}
-
-schema_install evince-thumbnailer-comics.schemas
-schema_install evince-thumbnailer-ps.schemas
-schema_install evince-thumbnailer.schemas
-schema_install evince-thumbnailer-djvu.schemas
-
 # update desktop entries
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
@@ -26,10 +13,5 @@ fi
 # run gsettings schemas conversion
 if [ -x /usr/bin/glib-compile-schemas ]; then
   /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas
-fi
-
-# Restart gconfd-2 if running to reload new gconf settings
-if ps acx | grep -q gconfd-2 ; then
-    killall -HUP gconfd-2 ;
 fi
 

264f01e67c8e3d438525e98f20b252cad779f5d5
Widya Walesa <walecha99@gmail.com>
2012-10-18 23:07:05 +0700

    * MPlayer: Update build script. * ORBit2: Added version 2.14.19. Needed by some GNOME2 packages. * ptbatterysystemtray: Upgraded to 1.0.0rc2. Not used in razorqt 0.5.0. * razorqt: Upgraded to 0.5.0. Fix environment handling and renaming init     script to razorqt instead razor. * clementine: Upgraded to git 20121001_07abe82. * fcgi: Added version 2.4.0. Needed to run Perl/CGI based script on nginx. * fcgiwrap: Added version 1.0.3. Needed to run Perl/CGI based script on nginx. * flash-player-plugin: Updated build script. * gconfmm: Added version 2.28.3. Needed by some GNOME2 packages. * gnome-common: Added version 2.34.0. Needed to build some GNOME2 packages. * gnome-mime-data: Added version 2.18.0. Needed by some GNOME2 packages. * gnome-vfs: Added version 2.24.4. Needed by some GNOME2 packages. * hotot: Upgraded to git version 9c8c8a2. * libbonobo: Updated build script. Needed by some GNOME2 packages. * libbonoboui: Updated build script. Needed by some GNOME2 packages. * libgnome: Added version 2.32.1. Needed by some GNOME2 package. * libgnomecanvas: Updated build script. Needed by some GNOME2 package. * libgnomeui: Added version 2.24.5. Needed by some GNOME2 packages. * libreoffice: Upgraded to 3.6.2.2. Fix build script. * libtorrent-rasterbar: Upgraded to 0.16.4. * libxml++: Upgraded to 2.34.2. Needed by some GNOME2 packages. * lxdm: Upgraded to 0.4.1. Fix init script. * me-tv: Added version 1.4.0.10. * qbittorrent: Upgraded to 3.0.5. * rekonq: Upgraded to 1.2. * tvtime: Added version 1.0.2. Use some patches from Fedora. * xawtv: Added version 3.102. * zapping: Tested version 0.10cvs6. Not working. * zvbi: Added version 0.2.33.

--- gconf-editor/doinst.sh
+++ gconf-editor/doinst.sh
@@ -13,7 +13,7 @@ if [ -x /usr/bin/update-desktop-database ]; then
 fi
 
 if [ -x /usr/bin/update-mime-database ]; then
-  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+  /usr/bin/update-mime-database /usr/share/mime >/dev/null 2>&1
 fi
 
 if [ -e /usr/share/icons/hicolor/icon-theme.cache ]; then

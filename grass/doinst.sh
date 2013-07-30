99144a9f611397a665409f2bd4af9e26920d7fff
w41l <walecha99@gmail.com>
2013-07-30 21:46:27 +0700

    Removed freexl, gdal, geos, grass, gsl, json-c, lapack, libfm, libspatialite, menu-cache, numpy, pcmanfm, postgis, proj, qgis. Use SBo packages.

--- grass/doinst.sh
+++
@@ -1,4 +0,0 @@
-sed -e '/\/opt\/grass-*/d' -e '/\/opt\/grass/d' -i /etc/ld.so.conf
-echo '/opt/grass/lib' >> etc/ld.so.conf
-sbin/ldconfig -r .
-
--- grass/grass.sh
+++
@@ -1,3 +0,0 @@
-export GISBASE=/opt/grass64
-export PATH=$PATH:$GISBASE/bin
-export MANPATH=$MANPATH:$GISBASE/man

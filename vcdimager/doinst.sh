32995a9c2cfc7c56f00c5f1218bc71be7dbf7fd8
Widya Walesa <walecha99@gmail.com>
2014-03-03 20:49:52 +0700

    Remove packages which already provided by SlackBuilds.org

--- vcdimager/doinst.sh
+++
@@ -1,6 +0,0 @@
-# Install new info files
-if [ -x usr/bin/install-info -a -d usr/info ]; then
-  usr/bin/install-info usr/info/vcdxrip.info.gz usr/info/dir >/dev/null 2>&1
-  usr/bin/install-info usr/info/vcdimager.info.gz usr/info/dir >/dev/null 2>&1
-  usr/bin/install-info usr/info/vcd-info.info.gz usr/info/dir >/dev/null 2>&1
-fi

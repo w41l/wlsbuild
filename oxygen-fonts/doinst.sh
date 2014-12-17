937c228080574bea9f2ee4f0b501e83d29f84ac3
w41l <walecha99@gmail.com>
2014-12-17 09:08:50 +0700

    * compton: Added version 0.1_beta2. * ipad_charge: Upgraded to version 20141007. * kdeconnect-kde: Testing. * libclc: Added version 20140501. * libdrm: Added back to build mesa-devel. * libinput: Added back to build wayland. * libomxil-bellagio: Added to build mesa-devel with omx support. * libunwind: Added version 1.1 to build lxqt. * libva-intel-driver: Upgraded to version 1.4.1. * libva-vdpau-driver: Patched to fix build with newer libva. * libva: Upgraded to version 1.4.1. * libvdpau: Added version 0.8. * libxkbcommon: Upgraded to version 0.5.0. * lxappearance: Upgraded to version 0.6.1. * mesa-demos: Upgraded to version 8.2.0. * mesa: Upgraded to version 10.4.0. Patched to fix broken debug facility. * mytop: Added version 1.6. * ocl-icd: Added version 2.2.3. * oxygen-fonts: Upgraded to version 5.0.0. * pcmanfm-qt: Upgraded to version 0.8.0. * qtmimetypes: Added version 1.0.1, need to build lxqt >= 0.8.0. * razorqt: Removed. Now LXQt. * sddm: Testing. * wayland: Pending, need systemd. * weston: Pending, need systemd. * wine: Upgraded to version 1.7.33 using wine-staging patches. * xf86-video-ati: Upgraded to version 20141121_ab950ab (git).

--- oxygen-fonts/doinst.sh
+++ oxygen-fonts/doinst.sh
@@ -1,4 +1,5 @@
 if [ -x /usr/bin/fc-cache ]; then
-    /usr/bin/fc-cache -f -s >/dev/null 2>&1
+  echo "Updating font-cache: /usr/bin/fc-cache -f -s";
+  /usr/bin/fc-cache -f -s >/dev/null 2>&1
 fi
 

3fd565e19df5fbe90ead92b57f1e7d127e31a265
Widya Walesa <walecha99@gmail.com>
2015-08-03 13:43:44 +0700

    * breeze-kde4: Added version 5.3.2. * cheser-icon-theme: Added version 3.16.1. * digikam: Upgraded to version 4.12.0. * geoclue: Renamed to geoclue2. * geoclue2: Added version 2.2.0. * libclc: Updated libclc.SlackBuild. * libomxil-bellagio: Updated libomxil-bellagio.SlackBuild * mesa: Updated mesa.SlackBuild. * ocl-icd: Updated ocl-icd.SlackBuild. * py3cairo: Updated py3cairo.SlackBuild. * py3gobject: Added version 3.14.0. * py3xdg: Updated py3xdg.SlackBuild. * wine: Build wine using CSMT + D3D9 patches. * xf86-video-ati: Rebuilt. * xf86-video-intel: Enable DRI3 support.

---
+++ cheser-icon-theme/doinst.sh
@@ -0,0 +1,5 @@
+if [ -r usr/share/icons/Cheser/icon-theme.cache ]; then
+  if [ -x usr/bin/gtk-update-icon-cache ]; then
+    usr/bin/gtk-update-icon-cache -q usr/share/icons/Cheser >/dev/null 2>&1
+  fi
+fi

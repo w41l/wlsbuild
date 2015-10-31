e9c1625862e10a8e877bf67e4789870305362efc
w41l <walecha99@gmail.com>
2015-10-31 09:45:00 +0700

    qupzilla: Upgraded to 1.8.8.

--- qupzilla/doinst.sh
+++ qupzilla/doinst.sh
@@ -1,14 +1,12 @@
-if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q /usr/share/applications >/dev/null 2>&1
+if [ -d usr/share/applications ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
 
-if [ -x /usr/bin/update-mime-database ]; then
-  /usr/bin/update-mime-database /usr/share/mime >/dev/null 2>&1
+if [ -d usr/share/mime ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
 
-if [ -e /usr/share/icons/hicolor/icon-theme.cache ]; then
-  if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache /usr/share/icons/hicolor >/dev/null 2>&1
-  fi
+if [ -f usr/share/icons/hicolor/icon-theme.cache ]; then
+  /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
 fi
 

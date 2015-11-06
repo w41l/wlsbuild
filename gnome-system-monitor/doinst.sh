9c9aae64189943ede43bb1cc7c0a31f5e6dfb1a3
w41l <walecha99@gmail.com>
2015-11-06 23:28:22 +0700

    Move GNOME related packages into another repo. Add upower (newer from stock package). Upgraded libwacom.

--- gnome-system-monitor/doinst.sh
+++
@@ -1,3 +0,0 @@
-if [ -x usr/bin/glib-compile-schemas -a -d usr/share/glib-2.0/schemas ]; then
-    usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas 1>/dev/null 2>/dev/null
-fi
--- yelp-xsl/doinst.sh
+++
@@ -1,4 +0,0 @@
-
-if [ -x /usr/bin/gtk-update-icon-cache -a -r /usr/share/icons/hicolor/icon-theme.cache ]; then
-    usr/bin/gtk-update-icon-cache -t -f usr/share/icons/hicolor 1>/dev/null 2>/dev/null
-fi

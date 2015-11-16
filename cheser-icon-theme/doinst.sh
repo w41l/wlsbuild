69a466ce46e8c1dcde3ef4dbdbb6f988be658e32
w41l <walecha99@gmail.com>
2015-11-16 13:20:23 +0700

    Add new packages: - ldns: 1.6.17 - pcsx2: 1.2.1_r5875
    Upgraded:
    - cheeser-icon-theme: 3.18.0

--- cheser-icon-theme/doinst.sh
+++ cheser-icon-theme/doinst.sh
@@ -1,5 +1,5 @@
 if [ -r usr/share/icons/Cheser/icon-theme.cache ]; then
-  if [ -x usr/bin/gtk-update-icon-cache ]; then
-    usr/bin/gtk-update-icon-cache -q usr/share/icons/Cheser >/dev/null 2>&1
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -q /usr/share/icons/Cheser >/dev/null 2>&1
   fi
 fi
